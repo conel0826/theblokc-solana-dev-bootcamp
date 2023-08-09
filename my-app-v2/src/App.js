81ea26e1d735fff2be9fa7a6d032b109b77f9285
conel0826 <conel0826@gmail.com>
2023-08-10 02:21:55 +0800

    v.2023.08.10.02.21

--- my-app-v2/src/App.js
+++ my-app-v2/src/App.js
@@ -214,7 +214,6 @@ function App() {
           href={link}
           target="_blank"
           rel="noopener noreferrer"
-          className="App-logo"
         >
           {displayText}
         </a>
